40665a1dad3506228cf4b7fc55414edbd125461c
Nelson <punkpunkada@gmail.com>
2022-03-30 18:40:56 -0300

    component media query

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -13,6 +13,11 @@ const Conteudo = styled.section`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  @media (max-width: 800px) {
+
+    flex-direction: column;
+  }
+
 `
 
 export default () => {
