ac59b2c228cdbfc7f327f509a8de591a7a15f103
Nelson <punkpunkada@gmail.com>
2022-03-31 13:36:07 -0300

    adicionando Icones

--- src/Components/Itens/index.jsx
+++ src/Components/Itens/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from 'styled-components'
 import Item from '../Item';
+import imageFilter from "../imageFilter/imageFilter";
+
 const Itens = styled.div`
     box-shadown: 4px 4px 20px 0px rgba(0,0,0,0.1);
     border-radius: 10px;
@@ -9,10 +11,11 @@ const Itens = styled.div`
     justify-content: space-around;
     padding: 10px;
     font-size: 12px
-`
+`;
 
 export default (props)=>{
     return <Itens>
+        {imageFilter(props.type)}
         <Item {...props}/>
         <span>{props.date}</span>
     </Itens>
