67642e13281f7ac210906fd3d13edefa7325b9e6
Nelson <punkpunkada@gmail.com>
2022-04-03 14:20:10 -0300

    estilizando themas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { GlobalStyle } from "./Components/GlobalStyle";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
-
+import { ThemeProvider } from 'styled-components';
+import { temaClaro, temaEscuro } from './Components/UI/temas'
 function App() {
   return (
-    <>
-    <GlobalStyle />
+
+    <ThemeProvider theme ={temaClaro}>
+      <GlobalStyle />
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -5,10 +5,11 @@ import Conta from "../Conta";
 import Extrato from "../Extrato";
 
 const Container = styled.div`
-  background-color: #f1f1f1;
+  background-color: ${({theme})=> theme.body};
   min-height: 90vh;
   padding: 0px 15vw;
-`
+  color: ${({theme})=> theme.text};
+`;
 
 const Conteudo = styled.section`
   display: flex;
