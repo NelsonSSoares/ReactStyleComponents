db5a7ced3c6dbcd7c9d68376edb3c176e58c0840
Nelson <punkpunkada@gmail.com>
2022-04-03 14:46:04 -0300

    botao de tema

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 import { GlobalStyle } from "./Components/GlobalStyle";
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
 import { ThemeProvider } from 'styled-components';
 import { temaClaro, temaEscuro } from './Components/UI/temas'
-function App() {
-  return (
+import { BtnTema } from "./Components/UI";
+import SwitcherThema from "./Components/SwitcherThema";
 
-    <ThemeProvider theme ={temaClaro}>
+
+
+function App() {
+
+const [tema, setTema] = useState(true);
+const toggleTema = () =>{
+  setTema((tema) => !tema)
+}
+  return (
+    
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitcherThema tema={tema} /> 
+      </BtnTema>
       <Cabecalho />
       <Container />
     </ThemeProvider>
